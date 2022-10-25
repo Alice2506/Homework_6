bf325c35b1d52fe9f1c49495715ca4aedb6884a8
Alice2506 <ralko84@mail.ru>
2022-10-25 16:42:38 +0300

    added corrections

--- Program.cs
+++ Program.cs
@@ -8,13 +8,40 @@
 // 3. посчитать их
 // 4. посчитать, сколько среди них больше 0
 
-int[] array = {0, 7, 8, -2, -2};
-for (int i = 0; i < array.Length; i++)
+Console.WriteLine("Введите количество чисел массива: ");
+int M = int.Parse(Console.ReadLine());
+int[] array = new int[M];
+
+void PrintArray(int[] col)
 {
-    int result = array[i];
-    if (result > 0)
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
     {
-        Console.WriteLine(result);
+        Console.Write(col[position] + " ");
+        position++;
     }
+}        
+PrintArray(array);
+Console.WriteLine();
+
+void FillArray(int[] collection)
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+        collection[i] = int.Parse(Console.ReadLine());
+    }
+}  
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int sumOfPositiveIndexes = 0;
+int index = 0;
+while (array[index] > 0)
+{
+    sumOfPositiveIndexes++;
+    index++;
 }
+Console.WriteLine("Количество положительных чисел, введенных пользователем: " + sumOfPositiveIndexes);
 
